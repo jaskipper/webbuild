169c8d80fab7c10000827e9772587f994b2dfd83
Jason Skipper <jason@skipperinnovations.com>
2016-11-07 16:16:36 -0500

    Font changes

--- web/app/themes/sage/templates/head.php
+++ web/app/themes/sage/templates/head.php
@@ -3,6 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Cabin+Condensed" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Cabin+Condensed|Cabin:400,700|Roboto:400,400i,700" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 </head>
